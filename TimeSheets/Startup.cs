c8377d07aa621ea7f0f382324ad69a4c8f36aef4
Дмитрий <semu-dima@yandex.ru>
2021-08-11 08:58:21 +0300

    :wrench: creating and configuring a project

--- TimeSheets/Startup.cs
+++ TimeSheets/Startup.cs
@@ -41,7 +41,11 @@ namespace TimeSheets
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TimeSheets v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TimeSheets v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
